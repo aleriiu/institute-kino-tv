4aa8c0280c158bd0c479d0bbbc468e9471c75eb5
aleriiu <aleriiiu@gmail.com>
2023-10-21 22:18:38 +0300

    added vert btns and mouse scroll

--- script.js
+++ script.js
@@ -69,3 +69,34 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 
     });
 });
+
+
+// mouse 
+
+let mouseDown = false;
+let startX, scrollLeft;
+const slider = document.getElementById('videos');
+
+const startDragging = (e) => {
+  mouseDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+}
+
+const stopDragging = (e) => {
+  mouseDown = false;
+}
+
+const move = (e) => {
+  e.preventDefault();
+  if(!mouseDown) { return; }
+  const x = e.pageX - slider.offsetLeft;
+  const scroll = x - startX;
+  slider.scrollLeft = scrollLeft - scroll;
+}
+
+// Add the event listeners
+slider.addEventListener('mousemove', move, false);
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
