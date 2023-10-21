3ec8be0cfa9c50fda220e64f6f9bb91ea9a675c9
aleriiu <aleriiiu@gmail.com>
2023-10-21 17:04:54 +0300

    added accordion, added btn news and small changes

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
 
+// banner
+
 let imgs = document.querySelectorAll('.banner-link');
 let dots = document.querySelectorAll('.hero__btn-click');
 
 let totalImgs = imgs.length;
-let imgPosition= 0;
+let imgPosition = 0;
 
 // Update Position
 function updatePosition() {
@@ -33,16 +35,37 @@ function updatePosition() {
 // Dot Position
 dots.forEach((dot, dotPosition) => {
     dot.addEventListener("click", () => {
-      imgPosition = dotPosition
-      updatePosition()
+        imgPosition = dotPosition
+        updatePosition()
     })
-  })
+})
 
-  setInterval( function (){
-    if (imgPosition === totalImgs -1){
-          imgPosition = 0;
-      } else{
-          imgPosition++;
-      }
-      updatePosition();
-  }, 5000)
+setInterval(function () {
+    if (imgPosition === totalImgs - 1) {
+        imgPosition = 0;
+    } else {
+        imgPosition++;
+    }
+    updatePosition();
+}, 5000)
+
+
+
+// accordion
+
+const accordionItemHeaders = document.querySelectorAll(".header__nav-item");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.querySelector(".menu-wrapper");
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = "50vh";
+        }
+        else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+
+    });
+});
